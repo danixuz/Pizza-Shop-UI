94e1738c57b75296acd5dc11c6d3e97441144639
danixuz <danixuz@icloud.com>
2022-08-02 13:48:01 +0300

    Initial Commit

---
+++ Pizza-Shop-UI/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Pizza-Shop-UI
+//
+//  Created by Daniel Spalek on 02/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pizza-Shop-UI/Pizza_Shop_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pizza_Shop_UIApp.swift
+//  Pizza-Shop-UI
+//
+//  Created by Daniel Spalek on 02/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Pizza_Shop_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
